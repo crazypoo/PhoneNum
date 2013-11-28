73237cde3e3829aae237d92f70776731b0fe244f
crazypoo <273277355@qq.com>
2013-11-28 15:36:30 +0800

    Initial Commit

---
+++ PhoneNum/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhoneNum
+//
+//  Created by crazypoo on 11/28/13.
+//  Copyright (c) 2013 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
